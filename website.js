f69d5ed9508d625d8d0f28541fca95d6d4f45297
rachellewis <rachlewis0000@gmail.com>
2017-04-24 15:05:35 -0600

    adding basic js

---
+++ website.js
@@ -0,0 +1,10 @@
+function logoOver(idd) {
+    document.getElementById(idd).style.filter = "opacity(70%)";
+}
+function logoOff(idd) {
+    document.getElementById(idd).style.filter = "opacity(20%)";
+}
+
+function testShow() {
+    document.getElementById('demo').style.display = 'block';
+}
